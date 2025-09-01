a86c2da8a2794695d1373351f504107720a8aa02
behnsport-oss <behnsport@gmail.com>
2025-09-01 19:02:10 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hallo Welt</h1>
+        <p>Deine Fibro-Balance App ist bereit!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
